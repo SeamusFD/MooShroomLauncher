d1b459a8db59767526f4ecd6d4087606f78f9045
Benjamin <armstrongmediahome@icloud.com>
2014-03-06 17:26:49 -0600

    Added Window
    Added Window and edited readme.md

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 MooShroomLauncher
 =================
 
-MooShroomLauncher a launcher for modpack compatible with Magic Mojo Loader, BlazeLoader, and Forge/FML
+MooShroomLauncher a launcher for modpack compatible with Magic Mojo Loader, BlazeLoader, and Forge/FML(This is WIP not all features are added.)
+
+
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
+import javax.swing.JFrame;
 /**
  * Created by SeamusFD on 3/6/14.
  * All rights belong to me!!
  */
 public class Main {
+    public static void main(String[] args) {
+        JFrame window1 = new JFrame("MooShroom Launcher");
+
+        window1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window1.setSize(1000, 550);
+        window1.setVisible(true);
+    }
 }
